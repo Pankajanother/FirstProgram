d2935452c4a556d3843e43c53b9469c996e55712
Pankajanother <pankajanother@gmail.com>
2018-04-11 13:06:49 +0530

    Commited

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ def my_function():
     print("A second string.")
 
 my_function()
+
+
+def my1_function(str1,str2):
+    print(str1)
+    print(str2)
+
+my1_function("This is argument1"," This is the second argument which is also the string")
+my1_function("Stringy","Hello World")
