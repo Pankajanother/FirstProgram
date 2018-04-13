047bf0c028f02d6831474d14ecc11e4b9e93c29d
Pankajanother <pankajanother@gmail.com>
2018-04-13 14:44:16 +0530

    Commited

--- attack.py
+++ attack.py
@@ -1,3 +1,4 @@
+'''
 import random
 
 playerhp = 260
@@ -17,3 +18,9 @@ while playerhp > 0 :
         print("You have low health. You've been teleported to the nearest inn.")
         break
 
+'''
+
+from Classes.enemy import Enemy
+
+enemy = Enemy(200, 60)
+print("HP:" , enemy.get_hp())
